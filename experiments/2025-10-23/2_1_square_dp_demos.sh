317315a34befed1162610086e3892e32adb0c833
ankile <lars.ankile@gmail.com>
2025-10-23 16:43:52 -0700

    Add the basic experiment run scripts

---
+++ experiments/2025-10-23/2_1_square_dp_demos.sh
@@ -0,0 +1,8 @@
+python -m sir.training.train_policy \
+        --repo-ids ankile/square-v1 \
+        --policy diffusion \
+        --save-video \
+        --env NutAssemblySquare \
+        --max-steps 600 \
+        --batch-size 128 \
+        --use-wandb
